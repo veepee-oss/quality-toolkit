f4dbd2f5743d15b03accb3bebdfd09ed2bc8ff32
jmfiaschi <jmfiaschi@veepee.com>
2021-04-02 18:12:14 +0200

    feat(sftp): add sftp client

---
+++ quality_toolkit/services/sftp.py
@@ -0,0 +1,40 @@
+import logging
+from pathlib import Path
+
+import pysftp
+
+
+class Sftp():
+
+    def __init__(self, host, username, password):
+        logging.getLogger('paramiko').setLevel(logging.WARNING)
+        cnopts = pysftp.CnOpts()
+        cnopts.hostkeys = None
+        self.sftp = pysftp.Connection(host, username, password, cnopts=cnopts)
+
+    def upload_file(self, local_file, remote_path):
+        """
+        Upload a local file into the sftp path
+        """
+        self.sftp.put(local_file, remote_path)
+
+    def list_files(self, remote_path, file):
+        """
+        List files into the remote path
+        """
+        list_files = []
+        self.sftp.cwd(remote_path)
+        list_elt = self.sftp.listdir()
+        for elt in list_elt:
+            p = Path(elt)
+            if self.sftp.isfile(elt) and elt.replace("".join(p.suffixes), "") == file:
+                self.sftp.remove(elt)
+            elif self.sftp.isfile(elt):
+                list_files.append(elt)
+        return list_files
+
+    def close(self):
+        """
+        Close the sftp connection
+        """
+        self.sftp.close()
