db853617a2bb2798c1aadf423bc1dfd0c3646d4b
Yohann VIRON <yviron@veepee.com>
2021-06-01 14:50:11 +0000

    feat(Mssql): add autocommit option

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="quality-toolkit",
-    version="1.4.1",
+    version="1.5.0-beta.1",
     packages=find_packages(),
     entry_points={},
     install_requires=[
