38133d3d7d92e1854d6fed14e002a76df4134a61
yohann <yviron@veepee.com>
2022-05-25 15:12:23 +0200

    feat(response_message): improve truncate

--- quality_toolkit/helpers/local_functions.py
+++ quality_toolkit/helpers/local_functions.py
@@ -55,8 +55,8 @@ def response_message(message, response, password=False, truncate_content=0, trun
     :param str message: A message to explain the request or the error
     :param requests.Response response: the request response
     :param bool password: if the header contain a secure data it will not show, by default it's false
-    :param integer truncate_content: truncate content to the defined size, 0 to deactivate, by default 0
-    :param integer truncate_body: truncate body to the defined size, 0 to deactivate, by default 0
+    :param integer truncate_content: truncate content to the defined size, -1 to deactivate, 0 to show all content, by default 0
+    :param integer truncate_body: truncate body to the defined size, -1 to deactivate, 0 to deactivate, by default 0
     """
     header = "the header contain a secure data, it can't to be showed"
     body = "No body"
@@ -64,11 +64,17 @@ def response_message(message, response, password=False, truncate_content=0, trun
         header = f"{response.request.headers}"
     if response.request.body is not None:
         body = response.request.body.decode('utf-8') if isinstance(response.request.body, (bytes, bytearray)) else response.request.body
-        if truncate_body != 0:
-            body = body[0:truncate_body]
+        if truncate_body != -1:
+            if truncate_body != 0:
+                body = body[0:truncate_body]
+        else:
+            body = "Don't want to show the body."
     content = response.content.decode('utf-8') if isinstance(response.content, (bytes, bytearray)) else response.content
-    if truncate_content != 0:
-        content = content[0:truncate_content]
+    if truncate_content != -1:
+        if truncate_content != 0:
+            content = content[0:truncate_content]
+    else:
+        content = "Don't want to show the content."
     return f"{message},\n\t" \
            f"- code: {response.status_code}\n\t" \
            f"- content: {content}\n\t" \
